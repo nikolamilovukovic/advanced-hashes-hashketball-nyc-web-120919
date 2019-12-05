348c0706337381d3809fcd412171bd5996398d35
Nikola Milovukovic <nikolamilovukovic@gmail.com>
2019-12-05 21:39:15 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,7 +1,7 @@
 def game_hash
   {
     :home => {  :team_name => "Brooklyn Nets",
-                :color => [ "Black", "White"],
+                :colors => [ "Black", "White"],
                 :players => [
                   {
                   :player_name => "Alan Anderson",
@@ -52,7 +52,7 @@ def game_hash
   
     
       :away => {  :team_name => "Charlotte Hornets",
-                  :color => [ "Turquoise", "Purple"],
+                  :colors => [ "Turquoise", "Purple"],
                   :players => [
                   {
                     :player_name => "Jeff Adrien",
