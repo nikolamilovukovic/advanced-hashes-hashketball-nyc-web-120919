7349d4f75e22b57c55a03e46aac2391188ac69e0
Nikola Milovukovic <nikolamilovukovic@gmail.com>
2019-12-05 21:28:17 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -95,9 +95,51 @@ def game_hash
                   :blocks => ,
                   :slam_dunks => , 
                   },
-
-
-
-
-
-]
+                  ],
+                    :home => {  :team_name => "Brooklyn Nets",
+                :color => [ "Black", "White"],
+                :players => [
+                  {
+                  :player_name => "Alan Anderson",
+                  :number => 0,
+                  :shoe => 16,
+                  :points => 22,
+                  :rebounds => 12,
+                  :assists => 12,
+                  :steals => 3,
+                  :blocks => 1,
+                  :slam_dunks => 1,
+                },
+                  {
+                  :player_name => "Reggie Evans",
+                  :number => ,
+                  :shoe => ,
+                  :points => ,
+                  :rebounds =>  ,
+                  :assists => ,
+                  :steals => ,
+                  :blocks => ,
+                  :slam_dunks => , 
+                  }, 
+                    {
+                  :player_name => "Reggie Evans",
+                  :number => ,
+                  :shoe => ,
+                  :points => ,
+                  :rebounds =>  ,
+                  :assists => ,
+                  :steals => ,
+                  :blocks => ,
+                  :slam_dunks => , 
+                  },
+                  {
+                  :player_name => "Reggie Evans",
+                  :number => ,
+                  :shoe => ,
+                  :points => ,
+                  :rebounds =>  ,
+                  :assists => ,
+                  :steals => ,
+                  :blocks => ,
+                  :slam_dunks => , 
+                  },
