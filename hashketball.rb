d6b9e0279293fc6e17904af36abfd648fa414cc0
Nikola Milovukovic <nikolamilovukovic@gmail.com>
2019-12-05 21:36:31 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -96,7 +96,7 @@ def game_hash
                     :steals => 1,
                     :blocks => 1,
                     :slam_dunks => 0, 
-                    },
+                    }
                   ]
       }
     }
