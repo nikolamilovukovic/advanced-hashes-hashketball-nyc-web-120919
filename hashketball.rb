771068e5215623e73e7f4daa3c93da953e057dd2
Nikola Milovukovic <nikolamilovukovic@gmail.com>
2019-12-05 21:18:43 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,8 +1,19 @@
 def game_hash
   {
-    :home => {  :team_name
-                :color
-
+    :home => {  :team_name => "Brooklyn Nets",
+                :color => [ "Black", "White"],
+                :players => [{
+                  :player_name => "A"
+                  :number =>
+                  :shoe =>
+                  :points =>
+                  :rebounds =>
+                  :assists =>
+                  :steals =>
+                  :blocks =>
+                  :slam_dunks =>
+                }
+                  ]
 
 
 
