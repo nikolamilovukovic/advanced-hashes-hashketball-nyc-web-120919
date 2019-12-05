50d2597b7e40556ff94131ad84d76fb18c2126e9
Nikola Milovukovic <nikolamilovukovic@gmail.com>
2019-12-05 22:52:19 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -79,7 +79,7 @@ def game_hash
                     :slam_dunks => 10
                   }, 
                     {
-                    :player_name => "Desanga Diop",
+                    :player_name => "DeSanga Diop",
                     :number => 2,
                     :shoe => 14,
                     :points => 24,
