dde7b6a7579112ff4ddf963a7794c9e47ac54ef7
Nikola Milovukovic <nikolamilovukovic@gmail.com>
2019-12-05 23:00:26 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -142,5 +142,20 @@ def game_hash
     end    
   end 
   
-  def shoe_size(players_names)
-    game_hash.each do |
+  def shoe_size(players_name)
+    game_hash.each do |place, team|
+        team.each do |attribute, data|
+        if attribute == :players 
+          data.each do |player|
+            if player[:player_name] == players_name
+              return player[:shoe]
+            end 
+          end 
+        end 
+      end    
+    end   
+    
+  end 
+    
+    
+    
