95829b0c70ef44e7b9abc9ddecf984c594032e8c
Nikola Milovukovic <nikolamilovukovic@gmail.com>
2019-12-05 21:24:22 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,28 +4,30 @@ def game_hash
                 :color => [ "Black", "White"],
                 :players => [
                   {
-                  :player_name => "Alan Anderson"
-                  :number => 0 
-                  :shoe => 16 
-                  :points => 22
-                  :rebounds => 12  
-                  :assists => 12 
-                  :steals => 3 
-                  :blocks => 1 
-                  :slam_dunks => 1
-                }
-                  ]
-                  :player_name => ""
-                  :number => 
-                  :shoe => 
-                  :points => 
-                  :rebounds =>   
-                  :assists => 
-                  :steals => 
-                  :blocks => 
-                  :slam_dunks => 
+                  :player_name => "Alan Anderson",
+                  :number => 0,
+                  :shoe => 16,
+                  :points => 22,
+                  :rebounds => 12,
+                  :assists => 12,
+                  :steals => 3,
+                  :blocks => 1,
+                  :slam_dunks => 1,
+                },
+                  {
+                  :player_name => "Reggie Evans",
+                  :number => ,
+                  :shoe => ,
+                  :points => ,
+                  :rebounds =>  ,
+                  :assists => ,
+                  :steals => ,
+                  :blocks => ,
+                  :slam_dunks => , 
+                  }
 
 
 
 
 
+]
