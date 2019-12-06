b1225dc88af24e7e5a5a03893d473790a1d1fc7f
Nikola Milovukovic <nikolamilovukovic@gmail.com>
2019-12-06 01:29:05 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -174,7 +174,7 @@ def player_numbers(team_name)
   game_hash.map do |place, team| 
     if team[:team_name] == team_name
       team.each do |attribute, data|
-      
+      data[:numbers]
       end 
     end
   end 
